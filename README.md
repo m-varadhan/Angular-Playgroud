eff4d9a83eb9478bcc2210f7e617a0edfe6d9d25
m-varadhan <varadhan.work@gmail.com>
2020-01-30 13:08:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-formgroup-control-setvalue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-formgroup-control-setvalue)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  form: FormGroup;
+
+  constructor(formBuilder: FormBuilder) {
+      this.form = formBuilder.group( {
+        username: ['12345678', [Validators.minLength(8), Validators.required] ],
+      })
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+    this.form.get('username').setValue('87654321');
+    }, 2000);
+  }
+}
