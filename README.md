46cb661a267a4b516930d005554246e55053260f
m-varadhan <varadhan.work@gmail.com>
2020-01-30 13:25:29 +0530

    ng-zone-demo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# ng-formgroup-control-setvalue
+# ng-zone-demo
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-formgroup-control-setvalue)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NgZoneDemoComponent } from './ng-zone-demo/ng-zone-demo.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, NgZoneDemoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
